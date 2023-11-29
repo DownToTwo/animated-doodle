a2ef478e90e428ed848b409f03746ee5e2291a07
DownToTwo <DownToTwo@users.noreply.github.com>
2023-11-29 15:31:51 +0000

    deploy: d73e085a23fef959cbd46d33733a6ef6cfd818e8

---
+++ precache-manifest.d40e7a6f07622d43280cca6369fe6952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb6aee8a749fae3b7d83b492035649d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e64679d01fbe8e05d87",
+    "url": "./static/css/main.69098c96.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "./static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "0e64679d01fbe8e05d87",
+    "url": "./static/js/main.ca71f86e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
