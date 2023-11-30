6160569fb389572e7b8abb8b6047976c5932c3a6
DownToTwo <yourf1493@gmail.com>
2023-11-30 02:18:46 +0200

    Make memorization for save todoList element

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -3,12 +3,43 @@ import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
-
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
 function App(props) {
+  const [filter, setFilter] = useState("All");
+  const [tasks, setTasks] = useState(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    return storedTasks;
+  });
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
+  useEffect(() => {
+    // Update filteredTasks whenever tasks or filter changes
+    const filtered = tasks.filter(FILTER_MAP[filter]);
+    setFilteredTasks(filtered);
+  }, [tasks, filter]);
+
+  useEffect(() => {
+    // Save tasks to localStorage whenever tasks change
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    // Calculate remaining tasks count after updating tasks
+    const remainingTasksCount = filteredTasks.filter(
+      (task) => !task.completed,
+    ).length;
+    const tasksNoun = remainingTasksCount !== 1 ? "tasks" : "task";
+    const headingText = `${remainingTasksCount} ${tasksNoun} remaining`;
+    listHeadingRef.current.textContent = headingText;
+  }, [tasks, filteredTasks]);
+
   function addTask(name) {
-    const newTask = { id: "id", name, completed: false };
+    const newTask = { id: name, name, completed: false };
     setTasks([...tasks, newTask]);
   }
+
   function deleteTask(id) {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
@@ -16,34 +47,23 @@ function App(props) {
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
-      // if this task has the same ID as the edited task
       if (id === task.id) {
-        // use object spread to make a new object
-        // whose `completed` prop has been inverted
         return { ...task, completed: !task.completed };
       }
       return task;
     });
     setTasks(updatedTasks);
   }
+
   function editTask(id, newName) {
     const editedTaskList = tasks.map((task) => {
-      // if this task has the same ID as the edited task
       if (id === task.id) {
-        //
         return { ...task, name: newName };
       }
       return task;
     });
     setTasks(editedTaskList);
   }
-  const [filter, setFilter] = useState("All");
-  const [tasks, setTasks] = useState(props.tasks);
-  const FILTER_MAP = {
-    All: () => true,
-    Active: (task) => !task.completed,
-    Completed: (task) => task.completed,
-  };
 
   const FILTER_NAMES = Object.keys(FILTER_MAP);
   const filterList = FILTER_NAMES.map((name) => (
@@ -55,36 +75,32 @@ function App(props) {
     />
   ));
 
-  const taskList = tasks
-    .filter(FILTER_MAP[filter])
-    .map((task) => (
-      <Todo
-        id={task.id}
-        name={task.name}
-        completed={task.completed}
-        key={task.id}
-        toggleTaskCompleted={toggleTaskCompleted}
-        deleteTask={deleteTask}
-        editTask={editTask}
-      />
-    ));
-  function usePrevious(value) {
-    const ref = useRef();
-    useEffect(() => {
-      ref.current = value;
-    });
-    return ref.current;
-  }
-  const prevTaskLength = usePrevious(tasks.length);
-  useEffect(() => {
-    if (tasks.length - prevTaskLength === -1) {
-      listHeadingRef.current.focus();
-    }
-  }, [tasks.length, prevTaskLength]);
+  const remainingTasksCount = filteredTasks.filter(
+    (task) => !task.completed,
+  ).length;
+  const tasksNoun = remainingTasksCount !== 1 ? "tasks" : "task";
+  const headingText = `${remainingTasksCount} ${tasksNoun} remaining`;
 
-  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
-  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   const listHeadingRef = useRef(null);
+  const MemoizedTodoList = React.memo(
+    ({ tasks, filter, toggleTaskCompleted, deleteTask, editTask }) => {
+      const taskList = tasks
+        .filter(FILTER_MAP[filter])
+        .map((task) => (
+          <Todo
+            id={task.id}
+            name={task.name}
+            completed={task.completed}
+            key={task.id}
+            toggleTaskCompleted={toggleTaskCompleted}
+            deleteTask={deleteTask}
+            editTask={editTask}
+          />
+        ));
+
+      return <>{taskList}</>;
+    },
+  );
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -100,7 +116,13 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        {taskList}
+        <MemoizedTodoList
+          tasks={tasks}
+          filter={filter}
+          toggleTaskCompleted={toggleTaskCompleted}
+          deleteTask={deleteTask}
+          editTask={editTask}
+        />
       </ul>
     </div>
   );
